150321f7d76f2225c82b90dfa3234fc09a7652d3
Md Sobuj Rana <sobuj42301@gmail.com>
2025-08-30 11:53:19 +0600

    implement donation and history button toggle system with responsive support

--- js/utilities.js
+++ js/utilities.js
@@ -8,17 +8,24 @@ const cardContainer = getElementById('card-container');
 const historySection = getElementById('history-section');
 
 
-historyBtn.addEventListener('click',function(){
-    console.log('hello history')
+historyBtn.addEventListener('click',function(event){
+    historyBtn.style.backgroundColor = "#B4F461"
+    donationBtn.style.backgroundColor ="white"
+    donationBtn.style.color = "black"
+    donationBtn.style.border = "1px solid gray"
     cardContainer.classList.add('hidden');
     historySection.classList.remove('hidden')
 })
 
 
-// donationBtn.addEventListener('click',function(){
-//     cardContainer.classList.remove('hidden')
-//     historySection.classList.add('hidden')
-// })
+donationBtn.addEventListener('click',function(){
+    donationBtn.style.backgroundColor = "#B4F461"
+    historyBtn.style.backgroundColor = "white"
+    historyBtn.style.color = "black"
+    historyBtn.style.border = "1px solid gray"
+    cardContainer.classList.remove('hidden')
+    historySection.classList.add('hidden')
+})
 
 function getElementById (id){
    return document.getElementById(id)
